f7926b3e29131bb4e49eecc879d9c70206394c6d
Quantao <75652473+quantaosun@users.noreply.github.com>
2022-08-14 10:39:01 +1000

    Google Colab terminal commands collection

--- Magpie.sh
+++ Magpie.sh
@@ -11,9 +11,9 @@
 
 ########################## INSTALL SOFTWARES ####################################
 # Install Conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh &&
-chmod +x https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh &&
-bash ./Miniconda3-py37_4.12.0-Linux-x86_64.sh -b -f -p /root/miniconda3 &&
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh 
+chmod +x Miniconda3-py37_4.12.0-Linux-x86_64.sh 
+bash ./Miniconda3-py37_4.12.0-Linux-x86_64.sh -b -f -p /root/miniconda3 
 
 # Starting conda
 export PATH=/root/miniconda3/bin:$PATH
